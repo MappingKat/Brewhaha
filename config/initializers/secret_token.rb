15f36bcb77880276bde57a6a927de03ffdb10493
Katrina Engelsted <katrina@engelsted.co>
2013-11-04 17:12:02 -0700

    creating rails structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Brewhaha::Application.config.secret_key_base = 'b6a3cdf3bddd574a2149860e4c85fc36f5f43b9dee1f9ad97ab1fb6fe9e3c6f3ca54558041b820e5cb5b25da39ce9d92646e1e14fc645e7ecff503d0e90e9887'
