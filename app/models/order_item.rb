beee8fcf88590e44c8e3a458ba60114396fd29ca
Brian Winterling <bwinterling@yahoo.com>
2013-11-05 21:20:19 -0700

    Added basic validations and sketched rspec

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,4 +2,9 @@ class OrderItem < ActiveRecord::Base
 
   belongs_to :items
 
+  validates :order_id, presence: true
+  validates :item_id, presence: true
+  validates :quantity, presence: true
+  validates :price, presence: true
+
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Order do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory"
+  it "is invalid without a status"
+  it "is invalid without a customer_id"
 end
