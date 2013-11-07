3b0831a6bc48dcbd0add413695c7ac59fb701526
Brian Winterling <bwinterling@yahoo.com>
2013-11-07 11:43:58 -0700

    Added Cart and CartItems

--- spec/features/user_items_spec.rb
+++ spec/features/user_items_spec.rb
@@ -4,6 +4,6 @@ describe 'item list' do
   it 'displays a list of items' do
     Item.new(name: "Man Beer", description: "This manly beer is for manly men.", price: 500, cost: 200).save
     visit '/items'
-    page.should have_content('beer')
+    page.should have_content('Beer')
   end
 end
