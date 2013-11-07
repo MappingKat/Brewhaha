dcff3712d69a3adfcacc304f117a4a0205d620e4
Brian Winterling <bwinterling@yahoo.com>
2013-11-06 21:11:08 -0700

    Fixed auth

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Brewhaha::Application.routes.draw do
 
   resources :customers
 
+  root to: 'categories#index'
+
   get 'auth/twitter/callback', to: 'sessions#create'
   # here is the omniauth callback route, which would be agnostic to provider
   # get '/auth/:provider/callback', to: 'sessions#create'
