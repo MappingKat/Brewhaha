30dc18f9eb26d3b7ef8c52e2971fa4d32af5576e
Katrina Engelsted <katrina@engelsted.co>
2013-11-06 17:08:52 -0700

    modifying customer, categories, and items model specs and validators

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,13 @@ class Item < ActiveRecord::Base
 
   belongs_to :category
 
-  validates :name, presence: true
-  validates :price, presence: true
-  validates :cost, presence: true
-
+  validates :name, 
+            presence: true, 
+            uniqueness: true
+  validates :description, 
+            presence: true
+  validates :price, 
+            presence: true
+  validates :cost, 
+            presence: true
 end
