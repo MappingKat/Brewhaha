0cb7526a524ed594d8306cefa207207e811951e1
QuillyT <QuillyT@gmail.com>
2013-11-13 14:43:39 -0700

    Fix send order layout, start on authorization.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,7 @@ class AdminController < ApplicationController
   # before_action :can_access
 
   def index
+    authorized_admin?
     # @orders = Order.all 
     # @users = User.all
     # @items = Item.all
