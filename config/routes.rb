7313e3fc6ec4e8158d01a13bea9b15d942204a36
QuillyT <QuillyT@gmail.com>
2013-11-13 00:43:20 -0700

    I think God helped us.

--- config/routes.rb
+++ config/routes.rb
@@ -34,9 +34,6 @@ Brewhaha::Application.routes.draw do
   delete 'orders/:id', to: 'orders#destroy'
   get 'orders/:id/confirm', to: 'orders#confirm', as: :confirm_order
 
-  get 'admin', to: 'admin#index', as: :admin_index
-  post 'admin_orders', to: 'admin_orders#show', as: :admin_orders
-
   get 'payment', to: 'payment#index', as: :payment
   get 'payment/confirm', to: 'payment#confirm', as: :payment_confirmation
 
