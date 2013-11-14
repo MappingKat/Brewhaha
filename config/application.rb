cf6b880ad4eca5d52c9db9467900f549c21010c9
Katrina Engelsted <katrina@engelsted.co>
2013-11-13 18:37:34 -0700

    getting images to render by adding line in application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,20 +1,21 @@
 require File.expand_path('../boot', __FILE__)
-require 'rails/all'
-Bundler.require(:default, Rails.env)
 
+require 'rails/all'
+
+Bundler.require(:default, Rails.env)
 
 module Brewhaha
   class Application < Rails::Application
-    config.assets.initialize_on_precompile = false 
-    config.generators do |g|
-      g.test_framework :rspec,
-        :fixtures => true,
-        :view_specs => false,
-        :helper_specs => false,
-        :routing_specs => true,
-        :controller_specs => true,
-        :request_specs => true
-      g.factory_girl dir: 'spec/factories'
-    end
+    config.assets.initialize_on_precompile = false
+    
+    #   g.test_framework :rspec,
+    #     :fixtures => true,
+    #     :view_specs => false,
+    #     :helper_specs => false,
+    #     :routing_specs => true,
+    #     :controller_specs => true,
+    #     :request_specs => true
+    #   g.factory_girl dir: 'spec/factories'
+    # end
   end
 end
